238e4f46749f3c281fe2473184d0344bbcb3b416
Hallie Wu <83149521+HuiqiongWu@users.noreply.github.com>
2022-09-10 22:15:39 -0400

    Update vending_machine.java

--- Vending-machine-project/vending_machine.java
+++ Vending-machine-project/vending_machine.java
@@ -1,41 +1,43 @@
 public class Vending_machine {
     private Item[][] items;
 
-    public vending_machine () {
-        this.items = new Items[items[0].length][items[1].length];
-        for (int i = 0; i < items[0].length; i++) {
-            for (int j; j < items[1].length; j++) {
+    public vending_machine (Item[][] items) {
+        this.items = new Items[items.length][items[0].length];
+        for (int i = 0; i < items.length; i++) {
+            for (int j = 0; j < items[i].length; j++) {
                 this.items[i][j] = new Item(items[i][j]);
             }
         }
     }
+    
+    public Item getItem(int row_index, int col_index) {
+        return new Item(this.items[row_index][col_index]);
+    }
 
     public void setItem(Item item, int row_index, int col_column) {
-        this.items[row_index][col_column] = item;
+        this.items[row_index][col_column] = new Item(item);
     }
 
     // sell action
-    public void sell(int row_index, int col_index) {
-        if (this.items[row_index][col_index].getQuantity() > 1) {
-            current_quantity = this.items[row_index][col_index].getQuantity();
-            this.items[row_index][col_index].setQuantity(current_quantity-1);
-        }
-        else {
-            this.items[row_index][col_index] = null;
-        }
+    public boolean dispense(int row_index, int col_index) {
+        if (this.items[row_index][col_index].getQuantity() > 0) {
+            items[row_index][col_index].setQuantity(items[row_index][col_index].getQuantity() - 1);
+            return true;
+        } 
+        return false;
     }
 
-    public String search(String name) {
-        for (i = 0; i < items[0].length; i++) {
-            for (j = 0; j < items[1].length; j++) {
-                if (this.items[i][j] == null) {
-                    continue;
-                }
-                else if (items[i][j].getName().equals(name) && items[i][j].getQuantity > 0) {
-                    return "\n" + this.items[i][j].getName().toString() + ":" + this.items[i][j].getPrice().toString() + this.items[i][j].getQuantity().toString();
-                }
+    public String toString() {
+
+        String temp = "";
+        for (int i = 0; i < items.length; i++) {
+            temp += "\t";
+            for (int j = 0; j < items[i].length; j++) {
+                temp += items[i][j].toString() + " ";
             }
+            temp += "\n\n";
         }
-        return "\nSorry the item is not available now";
+        temp += "\t************************************************";
+        return temp;
     }
 }
